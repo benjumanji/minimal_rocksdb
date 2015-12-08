d47ba1853fb94aa60e2d1616b61cb9263a96343a
Ben Edwards <edwards.benj@gmail.com>
2015-12-08 18:58:41 +0000

    still working, close to original

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ extern crate rocksdb;
 use rocksdb::{DB, DBVector, MergeOperands, Options, Writable, WriteBatch};
 use rocksdb::rocksdb_ffi::DBCFHandle;
 
-fn create_fresh(opts: &Options, path: &str) -> Result<DB, String> {
-    let mut db = try!(DB::open(&opts, path));
-    try!(db.create_cf("cf", &opts));
-    Ok(db)
+fn get_or_create(opts: &Options, db: &mut DB) -> Result<DBCFHandle, String> {
+    db.cf_handle("cf")
+        .map(|x| -> Result<DBCFHandle, String> { Ok(*x) })
+        .unwrap_or_else(|| { db.create_cf("cf", &opts) })
 }
 
 fn main() {
@@ -17,7 +17,7 @@ fn main() {
     let cfs = ["cf"];
     let path = "/tmp/foo";
 
-    let db = DB::open_cf(&opts, path, &cfs).or(create_fresh(&opts, path)).unwrap();
-    let intern_handle = *db.cf_handle(cf).ok_or("boom!").unwrap();
+    let mut db = DB::open_cf(&opts, path, &cfs).or(DB::open(&opts, path)).unwrap();
+    let intern_handle = get_or_create(&opts, &mut db);
     let iter = db.iterator_cf(cf).unwrap();
 }
